f7adffa4db44c95821965189192a0465471526d9
Zabashta_Artur <zarpost@gmail.com>
2022-10-17 13:35:28 +0300

    feat: add solution of simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+ function getClass(obj) {
+  return {}.toString.call(obj).slice(8, -1);
+}
 const chainMaker = {
+  chainArray: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return(this.chainArray.length);
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    value = '( ' + value + ' )';
+    this.chainArray.push(value)
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (getClass(position) !== "Number" || position < 1 || position > this.chainArray.length - 1){
+      this.chainArray = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    this.chainArray.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArray = this.chainArray.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const showChains = [...this.chainArray];
+    this.chainArray = [];
+    return showChains.join("~~");
   }
 };
 
